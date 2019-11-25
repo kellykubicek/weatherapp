abb18dc8d2bdc0c5c468f06dc76518a98834e27b
Kelly Kubicek <kellykubicek>
2019-11-25 16:00:51 -0600

    11/25/19

--- weather.js
+++ weather.js
@@ -15,8 +15,9 @@ $("#search_btn").click(function(){
     //variables for the OpenWeather API URL
     var apiLink = 'https://api.openweathermap.org/data/2.5/weather';
     var apiKey = 'd362ec41e6fff7208f6a3f55f7cb3c57';
+    var apiUnits = '&units=imperial';
     var zipInput = $('#zipCode').val();
-    var apiURL = apiLink + '?zip=' + zipInput + '&appid=' + apiKey;
+    var apiURL = apiLink + '?zip=' + zipInput + '&appid=' + apiKey + apiUnits;
     
     console.log(apiURL);
     
@@ -30,6 +31,22 @@ $("#search_btn").click(function(){
         var city = responseFromOW.name;
         var condition = responseFromOW.weather[0].description;
         
+    $('#city').html(responseFromOW.name);
+    $('#temp').html(responseFromOW.main.temp);
+        
+        if (responseFromOW.weather[0].main === 'Clouds') {
+            $('<img>').attr('src', '/img/cloudy.png').appendTo($('#weather_icon'));
+//            $('#weather_icon').attr('src', '/img/cloudy.png');
+        }  else if (responseFromOW.weather[0].main === 'Sunny') {
+             $('#weather_icon').html("background-image", "url(/img/sunny.jpeg)");
+        }
+    
+    $('#description').html(responseFromOW.weather[0].description);
+    $('#weather_icon').css("background-image", "url(/img/cloudy.png)");
+        
     });
+    
+    
+    
     
 });
